1ed8efb7882961c83298501a7168cd44b3ac8f81
kiana hee <kianahee25@gmail.com>
2023-11-09 01:33:32 -0800

    added counter for collectable

--- Assets/scripts/CollectObject.cs
+++ Assets/scripts/CollectObject.cs
@@ -1,14 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
 
 public class CollectObject : MonoBehaviour
 {
+
+    public TextMeshProUGUI countText;
+    private int count;
+    void SetCountText() 
+   {
+       countText.text =  "Count: " + count.ToString();
+   }
+    void Start(){
+    count = 0;
+    SetCountText();
+    }
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             Destroy(gameObject);
+            count = count + 1;
+            SetCountText();
         }
     }
 }
