37dec0663bc217ee4a5442a8a88e0a7fe25e3204
kennedy kang <kennedyekang@gmail.com>
2023-11-30 16:46:23 -0800

    updated animation stuff

--- Assets/scripts/SpikeReset.cs
+++ Assets/scripts/SpikeReset.cs
@@ -1,9 +1,25 @@
 using UnityEngine;
 
+
+
 public class SpikeReset : MonoBehaviour
 {
     // Position to reset the player to
     private Vector3 resetPosition = new Vector3(-6.88f, -2.48f, 0.03658727f);
+    public Animator move;
+
+    private void Start()
+    {
+        move.SetBool("isDead", false);
+    }
+
+    private void Update()
+    {
+        if (transform.position == resetPosition)
+        {
+            move.SetBool("isDead", false);
+        }
+    }
 
     // This function is called when this collider (attached to the player) enters another trigger collider
     private void OnTriggerEnter2D(Collider2D other)
@@ -11,6 +27,7 @@ public class SpikeReset : MonoBehaviour
         if (other.CompareTag("spike"))
         {
             // Reset this object's (player's) position
+            move.SetBool("isDead", true);
             transform.position = resetPosition;
         }
     }
