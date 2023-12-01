79d7cd3e255ef7f7ad310b4ac28d1754dfb59cf5
florence1z <malilaynicholas@gmail.com>
2023-11-30 16:32:02 -0800

    script to player
    script to player

--- Assets/scripts/SpikeReset.cs
+++ Assets/scripts/SpikeReset.cs
@@ -5,13 +5,13 @@ public class SpikeReset : MonoBehaviour
     // Position to reset the player to
     private Vector3 resetPosition = new Vector3(-6.88f, -2.48f, 0.03658727f);
 
-    // This function is called when another collider enters the trigger collider attached to this object
+    // This function is called when this collider (attached to the player) enters another trigger collider
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("spike"))
         {
-            // Reset player's position
-            other.transform.position = resetPosition;
+            // Reset this object's (player's) position
+            transform.position = resetPosition;
         }
     }
 }
