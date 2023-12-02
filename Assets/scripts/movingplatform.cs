424428d1d7b86ca5535ad587a473cee26fa0e7e5
florence1z <malilaynicholas@gmail.com>
2023-12-01 23:43:36 -0800

    Moving obstacle
    added moving obstacle script

---
+++ Assets/scripts/movingplatform.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : MonoBehaviour
+{
+    public Transform posA, posB;
+    public int Speed;
+    Vector2 targetPos;
+
+    void Start()
+    {
+        targetPos = posB.position;
+    }
+
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, posA.position) < .1f) targetPos = posB.position;
+        if (Vector2.Distance(transform.position, posB.position) < .1f) targetPos = posA.position;
+
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, Speed * Time.deltaTime);
+    }
+}
