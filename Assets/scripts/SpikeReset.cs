1e7f2ab1c0e31c26a898e3c9614f789a2bd108c5
kennedy kang <kennedyekang@gmail.com>
2023-12-01 17:33:44 -0800

    fixed the disappearing animation

--- Assets/scripts/SpikeReset.cs
+++ Assets/scripts/SpikeReset.cs
@@ -4,18 +4,30 @@ using UnityEngine.SceneManagement;
 public class SpikeReset : MonoBehaviour
 {
     public Animator move;
+    //public Rigidbody2D rb;
 
     private void Start()
     {
-        move.SetBool("isDead", false);
+        
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("spike"))
         {
-            move.SetBool("isDead", true);
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            Die();
         }
+    }
+
+    private void Die()
+    {
+        //rb.bodyType = RigidbodyType2D.Static;
+        move.SetTrigger("died");
+    }
+
+    private void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        //rb.bodyType = RigidbodyType2D.Dynamic;
     }
 }
