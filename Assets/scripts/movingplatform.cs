55a36122a3dd0922c38b238ee8f64cd041f89ed4
florence1z <malilaynicholas@gmail.com>
2023-12-03 22:53:10 -0800

    Push
    Pushing current changes, tweaked the script a little bit for the platform mover.

--- Assets/scripts/movingplatform.cs
+++ Assets/scripts/movingplatform.cs
@@ -7,16 +7,28 @@ public class PlatformController : MonoBehaviour
     public Transform posA, posB;
     public int Speed;
     Vector2 targetPos;
+    private Quaternion rotationAtPosA;
+    private Quaternion rotationAtPosB;
 
     void Start()
     {
         targetPos = posB.position;
+        rotationAtPosA = Quaternion.Euler(0, 0, 0);
+        rotationAtPosB = Quaternion.Euler(0, 180, 0);
     }
 
     void Update()
     {
-        if (Vector2.Distance(transform.position, posA.position) < .1f) targetPos = posB.position;
-        if (Vector2.Distance(transform.position, posB.position) < .1f) targetPos = posA.position;
+        if (Vector2.Distance(transform.position, posA.position) < .1f)
+        {
+            targetPos = posB.position;
+            transform.rotation = rotationAtPosB;
+        }
+        if (Vector2.Distance(transform.position, posB.position) < .1f)
+        {
+            targetPos = posA.position;
+            transform.rotation = rotationAtPosA;
+        }
 
         transform.position = Vector2.MoveTowards(transform.position, targetPos, Speed * Time.deltaTime);
     }
